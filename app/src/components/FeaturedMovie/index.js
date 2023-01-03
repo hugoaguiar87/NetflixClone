10ed9780157d3273a20fda99a435a38781903ecc
Hugo Aguiar <hugo-phone@live.com>
2023-01-02 22:49:58 -0300

    Componente Filme em Destaque foi estruturado. Falta estilizar.

--- app/src/components/FeaturedMovie/index.js
+++ app/src/components/FeaturedMovie/index.js
@@ -1,10 +1,36 @@
 import './style.css'
 
 const FeaturedMovie = ({item}) => {
+
+    const firstDate = new Date(item.first_air_date)
+    let genres = []
+
+    for(let i in item.genres) {
+        genres.push( item.genres[i].name )
+    }
+
     return (
-        <section className="featured" sty>
-            <div className="">
-                {item.original_name}
+        <section 
+            className="featured" 
+            style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`}}
+        >
+            <div className="featured--vertical">
+                <div className='featured--horizontal'>
+                    <div className='featured--name'>{item.original_name}</div>
+                    <div className='featured--info'>
+                        <div className='featured--points'>{item.vote_average} pontos</div>
+                        <div className='featured--year'>{firstDate.getFullYear()}</div>
+                        <div className='featured--seasons'>
+                            {item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}
+                        </div>
+                    </div>
+                    <div className='featured--description'>{item.overview}</div>
+                    <div className='featured--buttons'>
+                        <button className=''>► Assitir</button>
+                        <button>+ Minha Lista</button>
+                    </div>
+                    <div className='featured--genres'> <strong>Gêneros:</strong> {genres.join(', ')}</div>  
+                </div>
             </div>
         </section>
     )
