2a2418398f7708b45953afc25aeb8a6bbd7d58f4
Hugo Aguiar <hugo-phone@live.com>
2023-01-04 10:08:14 -0300

    Filme em destaque estilizado

--- app/src/components/FeaturedMovie/index.js
+++ app/src/components/FeaturedMovie/index.js
@@ -1,3 +1,5 @@
+// PAREI FALTANDO 1:22:34 PARA ACABAR A LIVE
+
 import './style.css'
 
 const FeaturedMovie = ({item}) => {
@@ -18,7 +20,7 @@ const FeaturedMovie = ({item}) => {
                 <div className='featured--horizontal'>
                     <div className='featured--name'>{item.original_name}</div>
                     <div className='featured--info'>
-                        <div className='featured--points'>{item.vote_average} pontos</div>
+                        <div className='featured--points'>{item.vote_average.toFixed(1)} pontos</div>
                         <div className='featured--year'>{firstDate.getFullYear()}</div>
                         <div className='featured--seasons'>
                             {item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}
@@ -26,8 +28,8 @@ const FeaturedMovie = ({item}) => {
                     </div>
                     <div className='featured--description'>{item.overview}</div>
                     <div className='featured--buttons'>
-                        <button className=''>► Assitir</button>
-                        <button>+ Minha Lista</button>
+                        <button className='featured--watchbutton'>► Assitir</button>
+                        <button className='featured--mylistbutton'>+ Minha Lista</button>
                     </div>
                     <div className='featured--genres'> <strong>Gêneros:</strong> {genres.join(', ')}</div>  
                 </div>
