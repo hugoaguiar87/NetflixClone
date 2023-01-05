759e41a3e7930f8c0689701e2b280fe9ecd0ecb7
Hugo Aguiar <hugo-phone@live.com>
2023-01-05 18:56:57 -0300

    Atualizado imagens

--- app/src/components/Header/index.js
+++ app/src/components/Header/index.js
@@ -1,17 +1,20 @@
 import './style.css'
 
+import LogoNetflix from '../../assets/netflixlogo.svg'
+import LogoUser from '../../assets/logouser.png'
+
 const Header = ({black}) => {
     return (
         <header className={black ? 'black' : ''}>
             <div className='header--logo'>
                 <a href='/'>
-                    <img src='https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg' alt='logo'/> 
+                    <img src={LogoNetflix} alt='logo'/> 
                 </a>
             </div>
 
             <div className='header--user'>
                 <a href='/'>
-                    <img src='https://obtainable-cabbage.surge.sh/static/media/logouser.6cd63fc3d72f015a2ebf.png' alt='user'/>
+                    <img src={LogoUser} alt='user'/>
                 </a>
             </div>
         </header>
